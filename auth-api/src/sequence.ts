59e236282809ff1957bef9e9f0395a7d65876ee4
Puskar Adhikari <puskar.ad@gmail.com>
2021-11-27 22:35:37 +0545

    added authentication action

--- auth-api/src/sequence.ts
+++ auth-api/src/sequence.ts
@@ -1,3 +1,51 @@
 import {MiddlewareSequence} from '@loopback/rest';
+import {Context, inject} from '@loopback/context';
+import {
+  AuthenticateFn,
+  AuthenticationBindings,
+  AUTHENTICATION_STRATEGY_NOT_FOUND,
+  USER_PROFILE_NOT_FOUND,
+} from '@loopback/authentication';
+import {
+  FindRoute,
+  InvokeMethod,
+  ParseParams,
+  Reject,
+  RequestContext,
+  RestBindings,
+  Send,
+  SequenceHandler,
+} from '@loopback/rest';
+const SequenceActions = RestBindings.SequenceActions;
+export class MySequence implements SequenceHandler {
+  constructor(
+    @inject(AuthenticationBindings.AUTH_ACTION)
+    protected authenticateRequest: AuthenticateFn,
+    @inject(RestBindings.Http.CONTEXT) public ctx: Context,
+    @inject(SequenceActions.FIND_ROUTE) protected findRoute: FindRoute,
+    @inject(SequenceActions.PARSE_PARAMS) protected parseParams: ParseParams,
+    @inject(SequenceActions.INVOKE_METHOD) protected invoke: InvokeMethod,
+    @inject(SequenceActions.SEND) public send: Send,
+    @inject(SequenceActions.REJECT) public reject: Reject,
+  ) {}
+  async handle(context: RequestContext) {
+    try {
+      const {request, response} = context;
+      const route = this.findRoute(request);
 
-export class MySequence extends MiddlewareSequence {}
+      await this.authenticateRequest(request);
+      const args = await this.parseParams(request, route);
+      const result = await this.invoke(route, args);
+      this.send(response, result);
+    } catch (err) {
+      if (
+        err.code === AUTHENTICATION_STRATEGY_NOT_FOUND ||
+        err.code === USER_PROFILE_NOT_FOUND
+      ) {
+        Object.assign(err, {statusCode: 401});
+      }
+
+      this.reject(context, err);
+    }
+  }
+}
