e32788fae28d8963742d692eb5b7c96fd10705e6
Puskar Adhikari <puskar.ad@gmail.com>
2021-11-30 23:05:56 +0545

    db connected to postgresql

--- server/src/datasources/db.datasource.ts
+++ server/src/datasources/db.datasource.ts
@@ -1,11 +1,18 @@
 import {inject, lifeCycleObserver, LifeCycleObserver} from '@loopback/core';
 import {juggler} from '@loopback/repository';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const config = {
   name: 'db',
-  connector: 'memory',
-  localStorage: '',
-  file: './data/db.json',
+  connector: 'postgresql',
+  url: '',
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 };
 
 @lifeCycleObserver('datasource')
