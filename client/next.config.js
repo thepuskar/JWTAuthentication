38752033a2b5fb1abdce1cd9b897e9908093949d
Puskar Adhikari <puskar.ad@gmail.com>
2021-12-02 12:19:30 +0545

    tailwind config with styled components and twin.macro

--- client/next.config.js
+++ client/next.config.js
@@ -1,4 +1,6 @@
-/** @type {import('next').NextConfig} */
 module.exports = {
-  reactStrictMode: true,
+  webpack: (config, { isServer }) => {
+    if (!isServer) config.resolve.fallback.fs = false
+    return config
+  }
 }
