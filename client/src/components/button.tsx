2c2098a6faa56055898331c07bd1916031e8babe
Puskar Adhikari <puskar.ad@gmail.com>
2021-12-14 14:11:49 +0545

    sping btn

--- client/src/components/button.tsx
+++ client/src/components/button.tsx
@@ -6,6 +6,34 @@ export const Button = (props: any) => {
   return <Btn {...props}>{props.text}</Btn>
 }
 
+export const SpinButton = (props: any) => {
+  return (
+    <SpinBtn type='button' disabled>
+      <svg
+        className='animate-spin -ml-1 mr-3 h-5 w-5 text-green-500'
+        xmlns='http://www.w3.org/2000/svg'
+        fill='none'
+        viewBox='0 0 24 24'
+      >
+        <circle
+          className='opacity-25'
+          cx='12'
+          cy='12'
+          r='10'
+          stroke='currentColor'
+          stroke-width='4'
+        ></circle>
+        <path
+          className='opacity-75'
+          fill='currentColor'
+          d='M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z'
+        ></path>
+      </svg>
+      {props.text}
+    </SpinBtn>
+  )
+}
+
 interface ButtonProps {
   rounded?: boolean
   green?: string
@@ -19,3 +47,15 @@ const Btn = styled.button(({ rounded, green, white }: ButtonProps) => [
   white && tw`border-white text-white  hover:bg-white hover:text-gray-500`,
   tw`border-2  px-12 py-2 inline-block font-semibold`
 ])
+
+const SpinBtn = styled.button`
+  ${tw`rounded-full text-green-500 inline-flex items-center border-2  px-7 py-2 font-semibold
+  border-green-500 leading-6  bg-white hover:bg-green-500 hover:text-white focus:border-green-500
+   active:bg-green-500
+   transition ease-in-out duration-150 cursor-not-allowed`};
+  &:hover {
+    svg {
+      color: white;
+    }
+  }
+`
