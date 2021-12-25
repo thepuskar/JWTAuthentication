8faa73ae5b86637b0a3e00713527c60e0490c180
Puskar Adhikari <puskar.ad@gmail.com>
2021-12-25 23:33:36 +0545

    error handler middleware

---
+++ server/src/middlewares/error-handler-middleware.ts
@@ -0,0 +1,18 @@
+import { Response, Request, NextFunction } from 'express';
+import { CustomError } from '../utils';
+
+export const errorHandlerMiddleware = (
+  err: Error,
+  _req: Request,
+  res: Response,
+  _next: NextFunction,
+) => {
+  if (err instanceof CustomError) {
+    res.status(err.statusCode).send({ errors: err.serializeErrors() });
+    return;
+  }
+  console.error(err);
+  res.status(400).send({
+    errors: [{ message: 'Something went wrong' }],
+  });
+};
