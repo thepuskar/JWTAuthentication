1c682fabba8e4e2b3da140a9bcb6e7ba207991a2
Puskar Adhikari <puskar.ad@gmail.com>
2021-12-25 23:16:48 +0545

    custome error message

---
+++ server/src/utils/errors/custome-error.ts
@@ -0,0 +1,13 @@
+import { ErrorMessage } from '../../interface';
+
+export abstract class CustomError extends Error {
+  abstract statusCode: number;
+
+  constructor(message: string) {
+    super(message);
+
+    Object.setPrototypeOf(this, CustomError.prototype);
+  }
+
+  abstract serializeErrors(): ErrorMessage[];
+}
