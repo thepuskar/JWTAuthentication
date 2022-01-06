fcf9aab4578a96d20bc03f54b122b30cab2460cc
Puskar Adhikari <puskar.ad@gmail.com>
2022-01-06 12:34:53 +0545

    reset and activation token generated

--- server/src/utils/token.generator.ts
+++ server/src/utils/token.generator.ts
@@ -1,5 +1,16 @@
 import jwt from 'jsonwebtoken';
+import crypto from 'crypto';
 
 export const TokenGenerator = (id: string, email: string) => {
   return jwt.sign({ id, email }, process.env.JWT_KEY!);
+};
+
+export const genAccountActivationToken = () => {
+  const randomBytes = crypto.randomBytes(32).toString('hex');
+  return crypto.createHash('sha256').update(randomBytes).digest('hex');
+};
+export const genPasswordResetToken = () => {
+  const token = genAccountActivationToken();
+  const tokenExpiration = Date.now() + 10 * 60 * 1000;
+  return { token, tokenExpiration };
 };
