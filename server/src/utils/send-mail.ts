3f91b78cbf10b39cc9c88cfb637b00b68d668515
Puskar Adhikari <puskar.ad@gmail.com>
2022-01-19 16:09:47 +0545

    sending mail updated

--- server/src/utils/send-mail.ts
+++ server/src/utils/send-mail.ts
@@ -1,11 +1,24 @@
 import nodemailer from 'nodemailer';
 import { BadRequestError } from './errors';
+import { renderFile } from 'ejs';
 
-export const sendMail = async (
-  email: string,
-  subject: string,
-  text: string,
-) => {
+interface IOptions {
+  user: object;
+  url: string;
+  template: string;
+  date?: Date;
+  email: string;
+  subject: string;
+}
+
+export const sendMail = async ({
+  user,
+  url,
+  template,
+  date,
+  email,
+  subject,
+}: IOptions) => {
   try {
     const transporter = nodemailer.createTransport({
       host: process.env.EMAIL_HOST,
@@ -17,13 +30,24 @@ export const sendMail = async (
         pass: process.env.EMAIL_PASS,
       },
     });
+
+    const html = await renderFile(
+      `${__dirname}/../../views/email/${template}`,
+      {
+        user,
+        url,
+        date,
+      },
+    );
+
     await transporter.sendMail({
       from: process.env.EMAIL_USER,
       to: email,
       subject,
-      text,
+      html,
     });
   } catch (error) {
+    console.log(error);
     throw new BadRequestError('Email sending failed');
   }
 };
